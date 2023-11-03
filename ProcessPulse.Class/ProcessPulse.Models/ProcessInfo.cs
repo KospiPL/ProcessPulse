d483fb4201e5489034ce04bc802df0c7bc0dda5f
KospiPL <kacper.cudzik11@gmail.com>
2023-11-03 10:09:14 +0100

    Działające Api i odczyty z niego

--- ProcessPulse.Class/ProcessPulse.Models/ProcessInfo.cs
+++ ProcessPulse.Class/ProcessPulse.Models/ProcessInfo.cs
@@ -11,16 +11,16 @@ namespace ProcessPulse.BibliotekaKlas.ProcessPulse.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int Id_Process { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public double CpuUsage { get; set; }
         public double RamUsage { get; set; }
-        public string Path { get; set; }
+        public string? Path { get; set; }
         public double NetworkUsage { get; set; }
-        public string TerminalId { get; set; }
+        public string? TerminalId { get; set; }
         public DateTime Time { get; set; }
         public int? ParentId { get; set; }
         [ForeignKey("ParentId")]
-        public ProcessInfo ParentProcess { get; set; }
+        public ProcessInfo? ParentProcess { get; set; }
         public List<ProcessInfo> ChildProcesses { get; set; } = new List<ProcessInfo>();
     }
 }
