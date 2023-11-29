da7a85824a31078907562d1706e548b5413c1ed2
KospiPL <kacper.cudzik11@gmail.com>
2023-11-29 10:04:50 +0100

    Gotowa strona logowania do aplikacji i integracja z GC.Auth

--- ProcessPulse.App/MauiProgram.cs
+++ ProcessPulse.App/MauiProgram.cs
@@ -1,12 +1,13 @@
 ﻿using Blazorise;
 using Blazorise.Bootstrap5;
-using Microsoft.EntityFrameworkCore;
+using Blazorise.Icons.FontAwesome;
+using GC.Auth.Permissions;
+using GearCode.Common.Auth.Forms;
 using Microsoft.Extensions.Logging;
 using ProcessPulse.App.Data;
 using ProcessPulse.App.Services;
-using ProcessPulse.Class.Service;
-using Blazorise.Icons.FontAwesome;
 using ProcessPulse.Class.ProcessPulse.Models;
+using ProcessPulse.Class.Service;
 
 namespace ProcessPulse.App
 {
@@ -22,24 +23,51 @@ namespace ProcessPulse.App
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            // Inicjalizacja IdentityManager
+            InitializeIdentityManager();
+
             builder.Services.AddMauiBlazorWebView();
             builder.Services.AddBlazorise(options =>
-                            {
-                             options.Immediate = true;
-                             })
-                             .AddBootstrap5Providers()
-                             .AddFontAwesomeIcons();
+            {
+                options.Immediate = true;
+            })
+            .AddBootstrap5Providers()
+            .AddFontAwesomeIcons();
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
 #endif
+
             builder.Services.AddHttpClient();
             builder.Services.AddScoped<ApiService>();
-            builder.Services.AddScoped< ITerminalService , TerminalService > ();
+            builder.Services.AddScoped<ITerminalService, TerminalService>();
             builder.Services.AddSingleton<WeatherForecastService>();
 
             return builder.Build();
+        }
+
+        private static void InitializeIdentityManager()
+        {
+           
+            var config = new IdentityManagerConfig(
+                title: "Auth",
+                clientAppCode: "GC.CART", 
+                resourceAppCode: "GC.CATALOG.API", 
+                languageCode: "pl", 
+                environment: AuthEnvironment.Production, 
+                sessionPath: null, 
+                forcedTenant: null
+            );
+
+           
+            IdentityManager.Initialize(
+                config: config,
+                permissionsMapper: new PermissionsMapperByAttributes(typeof(GearCodePermissions)),
+                logger: null 
+            );
+
+            
         }
     }
 }
